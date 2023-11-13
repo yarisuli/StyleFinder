c06f28fe0f91d4be623827eff25953985c160976
yarisuli <148562678+yarisuli@users.noreply.github.com>
2023-11-13 17:33:41 -0300

    CREACION API CLIMA

---
+++ ClimaAPIConnector.cs
@@ -0,0 +1,39 @@
+﻿using RestSharp;
+using System;
+
+namespace App_Ropa___Intento_1
+{
+    public class ClimaAPIConnector
+    {
+        private static readonly string apiKey = System.Configuration.ConfigurationSettings.AppSettings["WeatherApi"];
+        public static Pronostico getClima(DateTime diaHora, double latitud, double longitud)
+        {
+            // URL del servicio REST
+            string baseApiUrl = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services";
+            string metodo = "/timeline/${ubicacion}/${diaHora}?unitGroup=metric&include=events&key=${apiKey}&contentType=json&lang=es&iconSet=icons2&include=current";
+
+            string diaHoraStr = diaHora.ToString("yyyy-MM-ddTHH:mm:ss");
+            string ubicacionStr = latitud.ToString("0.#####").Replace(",", ".") + ", " + longitud.ToString("0.#####").Replace(",", ".");
+            
+            metodo = metodo.Replace("${ubicacion}", ubicacionStr).Replace("${diaHora}", diaHoraStr).Replace("${apiKey}", apiKey);
+
+            var options = new RestClientOptions(baseApiUrl)
+            {
+                ThrowOnAnyError = true
+            };
+            var restClient = new RestClient(options);
+            var request = new RestRequest(metodo);
+
+            RestResponse<Pronostico> response = restClient.ExecuteGet<Pronostico>(request);
+             
+            if (!response.IsSuccessful)
+            {
+                Console.WriteLine("Error" + response.StatusDescription);
+            }
+
+            return response.Data;
+
+        }
+    }
+}
+
--- SituacionOutfit.cs
+++ SituacionOutfit.cs
@@ -13,24 +13,11 @@ namespace App_Ropa___Intento_1
             this.buttonSalir.Click += (sender, EventArgs) => { this.buttonSituacion_Click(sender, EventArgs, Constantes.Situacion.Salir); };
             this.buttonCasual.Click += (sender, EventArgs) => { this.buttonSituacion_Click(sender, EventArgs, Constantes.Situacion.Casual); };
             this.buttonDeporte.Click += (sender, EventArgs) => { this.buttonSituacion_Click(sender, EventArgs, Constantes.Situacion.Deporte); };
-
-
-            //  string sql = "SELECT pais FROM usuario WHERE user_id = @usuario";
-            //  OleDbParameter[] parameters = new OleDbParameter[]
-            // {
-            // new OleDbParameter("@usuario", LogInfo.UserID),};
-
-            // DataTable dt = DB.GetDataTable(sql, parameters);
-
-            // string dtString = Convert.ToString(dt);
-            //  MessageBox.Show(dtString);
-
-            //var client = new RestClient("http://api.weatherapi.com/v1");
-            //var request = new RestRequest();
-            //  request.AddHeader("key", "e8cccdccdeeb41dcb99114709230811");
-            //  request.AddHeader("q","PAIS DEL USUARIO CON LATITUD ETC") ;
-
-
+            
+            double latitud = -34.61315;
+            double longitud = -58.37723;
+            
+            Pronostico pronostico = ClimaAPIConnector.getClima(DateTime.Now, latitud, longitud);
         }
 
 
@@ -58,8 +45,8 @@ namespace App_Ropa___Intento_1
             home.Show();
         }
 
-    }
 
+    }
         
 }
  
